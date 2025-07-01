7496e64f49dd62f7fa1835f2ebd2582680cfb323
asyylz <asiyeyaliz@gmail.com>
2025-07-01 14:21:14 +0100

    Refactor HTML templates for consistency and improve password view layout

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -2,29 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
+
 # Create your views here.
 def home(request):
-    return render(request,'generator/home.html')
+    return render(request, "generator/home.html")
+
 
 def password(request):
-    print('request',request.GET.get('length',12))
-    characters=list('abcdefghijklmnopqrstuvwxyz')
-    length=int(request.GET.get('length',12))
-    thepassword=''
-    if request.GET.get('uppercase'):
-            characters.extend(list('ABCDEFGHIGKLMNOPQRSTVUWXYZ'))
+    print("request", request.GET.get("length", 12))
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+    length = int(request.GET.get("length", 12))
+    thepassword = ""
+    if request.GET.get("uppercase"):
+        characters.extend(list("ABCDEFGHIGKLMNOPQRSTVUWXYZ"))
 
-    if request.GET.get('specials'):
-            characters.extend(list('£$%^&*(@!)'))
-            
-    if request.GET.get('numbers'):
-            characters.extend(list('0123456789'))
+    if request.GET.get("specials"):
+        characters.extend(list("£$%^&*(@!)"))
+
+    if request.GET.get("numbers"):
+        characters.extend(list("0123456789"))
 
     for x in range(length):
-        thepassword+=random.choice(characters)
+        thepassword += random.choice(characters)
 
-    return render(request,'generator/password.html',{'password':thepassword})
+    return render(request, "generator/password.html", {"password": thepassword})
+
 
 def aboutus(request):
-    return render(request,'generator/aboutus.html')
-    
+    return render(request, "generator/aboutus.html")
