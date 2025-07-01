74ca5cc057c9413aafae53feba0ddb65389f900c
asyylz <asiyeyaliz@gmail.com>
2025-07-01 19:56:25 +0100

    Initialize personal portfolio project with Django setup, including ASGI and WSGI configurations, settings, URL routing, and portfolio app with Project model, views, and templates.

---
+++ personal_portfolio-project/portfolio/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Project
+# Register your models here.
+
+#We say hey, I want to see this model inside of th admin.
+admin.site.register(Project)
